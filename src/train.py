7d7d03070664697c191eb09e003e136acd04292c
Israelujan <A01794693@tec.mx>
2024-10-20 16:30:23 -0600

    Cambiod para correr pipeline con mlflow

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 import joblib
 import mlflow
 
@@ -13,12 +14,18 @@ def load_params():
 
 params = load_params()
 
-def train_model(X_train_path, y_train_path, model_type):
+def train_model(X_train_path, y_train_path, model_type, X_test_path, y_test_path,):
     X_train = pd.read_csv(X_train_path)
     y_train = pd.read_csv(y_train_path).values.ravel()
-
+    X_test = pd.read_csv(X_test_path)
+    y_test = pd.read_csv(y_test_path)
     model_params = params['models'][model_type]
-
+    y_pred = model.predict(X_test)
+        # Calculate metrics
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred, average='weighted')
+    rec = recall_score(y_test, y_pred, average='weighted')
+    
     if model_type == 'logistic_regression':
         model = LogisticRegression(**model_params)
     elif model_type == 'random_forest':
@@ -38,8 +45,10 @@ if __name__ == '__main__':
     X_train_path = sys.argv[1]
     y_train_path = sys.argv[2]
     model_type = sys.argv[3]
+    X_test_path = sys.argv[4]
+    y_test_path = sys.argv[5]
     model_dir = params['data']['models']
     model_path = f"{model_dir}/{model_type}_model.pkl"
 
-    model = train_model(X_train_path, y_train_path, model_type)
+    model = train_model(X_train_path, y_train_path, X_test_path, y_test_path, model_type)
     joblib.dump(model, model_path)
